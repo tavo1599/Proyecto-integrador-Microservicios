4ff7a14fd49385e133d442861ca01146e8bfab63
tavo1599 <gustavo08_99@hotmail.com>
2023-06-24 20:34:08 -0500

    config

---
+++ Refistry-Service/src/test/java/com/quispcs/refistryservice/RefistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quispcs.refistryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
